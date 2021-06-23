6205a1a785a3e0564d8001be3fbcae18356da405
a-fujimt <a-fujimt@ist.osaka-u.ac.jp>
2021-06-23 12:38:40 +0900

    add card in tree

--- components/TreeComponent.js
+++ components/TreeComponent.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  cardcontent: {
+    padding: 16, // default value
+    "&:last-child": {
+      paddingBottom: 16 // default value is 24
+    }
+  },
+}));
 
 export const TreeComponent = (tree) => {
+
+  const classes = useStyles();
+  const onMediaFallback = event => event.target.src = "/noimage.png";
+
   return (
     <li>
-      <span>{tree.title}</span>
+      <Card variant="outlined">
+        <Grid container spacing={1} alignItems="center">
+          <Grid item xs={8}>
+            <CardContent className={classes.cardcontent}>
+              <Typography variant="h5" component="h2">
+                {tree.title}
+              </Typography>
+              <Typography variant="body1" color="textSecondary" component="text">
+                {tree.name}
+              </Typography>
+            </CardContent>
+          </Grid>
+          <Grid item xs={4}>
+            <CardMedia
+              className={classes.media}
+              image="/noimage.png"
+              title="Image"
+              onError={onMediaFallback}
+            />
+          </Grid>
+        </Grid>
+      </Card>
       <ul>
         {tree.hasOwnProperty('next') && tree['next'].map((item) => (
           <TreeComponent key={item.id} {...item} />
