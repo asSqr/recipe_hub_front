1f444645110f0ae33ef3268b0b453c9f662de220
g20tk009 <g20tk009@gmail.com>
2021-06-23 16:47:33 +0900

    created new recipe card

---
+++ components/preview.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Link from 'next/link';
+import RichEditorExample from './markdown'
+
+const useStyles = makeStyles({
+  root: {
+    width: '20rem',
+    height: '20rem',
+    textAlign: 'center',
+    position: 'relative'
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  footer: {
+    position: 'absolute',
+    bottom: '5px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  }
+});
+
+export const unescapeHtml = target => {
+    if( typeof target !== 'string' )
+      return target;
+  
+    const patterns = {
+      '&lt;'   : '<',
+      '&gt;'   : '>',
+      '&amp;'  : '&',
+      '&quot;' : '"',
+      '&#x27;' : '\'',
+      '&#x60;' : '',
+      '&#x2F;'  : '/',
+    };
+  
+    return target.replace(/&(lt|gt|amp|quot|#x27|#x60|#x2F);/g, match => {
+      return patterns[match];
+    });
+};
+
+const RecipeItem = (props) => {
+  const { id, name, recipe, id_fork_from, show_link } = props;
+
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          レシピ名
+        </Typography>
+        <Typography variant="h5" component="h2">
+          {name}
+        </Typography>
+        <br /> <br />
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          レシピ
+        </Typography>
+        {/* <Typography variant="body2" component="p">
+          {recipe}
+        </Typography> */}
+        <div
+          className="article-body"
+          dangerouslySetInnerHTML={{
+            __html: `${unescapeHtml(recipe)}`,
+          }}
+        />
+
+      </CardContent>
+      {show_link ?
+        <Link href={`/recipe/${id}`}>
+          <Button 
+            variant="contained"
+            color="primary"
+            className={classes.footer}
+          >
+            レシピページへ
+          </Button>
+        </Link>
+      : 
+        <></>}
+    </Card>
+  );
+};
+
+export default RecipeItem;
--- pages/recipe/[id].js
+++ pages/recipe/[id].js
@@ -4,7 +4,8 @@ import { useRouter } from 'next/router';
 import styles from '../../styles/Home.module.css'
 import React, { useEffect, useState } from 'react';
 import { fetchRecipe, postFork } from '../../utils/api_request';
-import RecipeItem from '../../components/RecipeItem';
+// import RecipeItem from '../../components/RecipeItem';
+import RecipeItem from '../../components/preview';
 
 export default function Recipe() {
   const nameRef = React.createRef();
